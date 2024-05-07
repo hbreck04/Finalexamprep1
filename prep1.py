42a456777a207cdcbb624ddd1adeb0f910795bd0
hbreck04 <breckheiha05@uww.edu>
2024-05-07 12:25:58 -0500

    first commit

---
+++ prep1.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    return fib_sequence
+
+fib_numbers = fibonacci(5)
+for num in fib_numbers:
+    print(num)
